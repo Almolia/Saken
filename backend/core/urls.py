2eb21c7019e89ff87af5150c6bc25aadc56d99f6
mojtaba1382 <68891572+mojtaba1382@users.noreply.github.com>
2026-07-08 15:56:13 +0330

    add building urls to core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.api_urls")),
+    path("api/", include('buildings.urls')),
 ]
