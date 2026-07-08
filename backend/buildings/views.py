96a2410d389ef2780f7a36e5b167ed5934248269
mojtaba1382 <68891572+mojtaba1382@users.noreply.github.com>
2026-07-08 15:53:49 +0330

    add view for buildings

--- backend/buildings/views.py
+++ backend/buildings/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Unit
+from .serializers import UnitSerializer
 
-# Create your views here.
+
+class MyUnitView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        units = user.units.all()
+        if not units.exists():
+            return Response(
+                {"detail": "No unit assigned to this user."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        if units.count() == 1:
+            serializer = UnitSerializer(units.first())
+        else:
+            serializer = UnitSerializer(units, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
