e1d57d1065d31e7bb818422681e08b2ac189d872
matadysa <matadysa@gmail.com>
2026-07-26 23:46:37 +0330

    add root urls and apply migration

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.api_urls")),
     path("api/", include('buildings.urls')),
+    path("api/", include('maintenance.urls')),
 ]
