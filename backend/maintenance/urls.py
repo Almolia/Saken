ded9c5025fc546b3e0aa2d448483b9058fe5ca84
mojtaba1382 <68891572+mojtaba1382@users.noreply.github.com>
2026-07-31 15:50:08 +0330

    Add new url for manager service request detail view

--- backend/maintenance/urls.py
+++ backend/maintenance/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     ManagerServiceRequestAssignView,
     ManagerServiceRequestListView,
+    ManagerServiceRequestDetailView,
     ServiceRequestListCreateView,
 )
 
 urlpatterns = [
     path('requests/', ServiceRequestListCreateView.as_view(), name='service-request-list'),
     path('manager/requests/', ManagerServiceRequestListView.as_view(), name='manager-service-request-list'),
+    path('manager/requests/<int:pk>/', ManagerServiceRequestDetailView.as_view(), name='manager-service-request-detail'),
     path('manager/requests/<int:pk>/assign/', ManagerServiceRequestAssignView.as_view(), name='manager-service-request-assign'),
 ]
